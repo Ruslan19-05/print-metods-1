b1a146ce8696a28ad45801459ab7925aac489005
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-10-25 22:57:26 +0400

    eight types methods

---
+++ src/main/java/az/edu/turing/Main.java
@@ -0,0 +1,19 @@
+package az.edu.turing;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("byte - 2 üstü 8 tam ədələr ");
+        System.out.println("short-2 üstü 16 tam ədələr ");
+        System.out.println("int-2 üstü 32 tam ədədlər");
+        System.out.println("long - 2 üstü 64 tam ədədlər");
+        System.out.println("float-2 üstü 32 -- kəsir ədədlər ");
+        System.out.println("double-2 üstü 64 --kəsir ədədlər ");
+        System.out.println("char-simvol");
+        System.out.println("boolean - true/false");
+
+
+
+    }
+}
